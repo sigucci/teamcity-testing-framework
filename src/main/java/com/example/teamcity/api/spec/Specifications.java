b4b7f657a44167f69a901d44b4e5faf068147a4c
sigucci <sig.tch@yandex.ru>
2025-04-07 22:31:28 +0300

    Add CI

--- src/main/java/com/example/teamcity/api/spec/Specifications.java
+++ src/main/java/com/example/teamcity/api/spec/Specifications.java
@@ -1,14 +1,19 @@
 package com.example.teamcity.api.spec;
 
+import java.nio.file.Paths;
 import java.util.List;
 
 import com.example.teamcity.api.config.Config;
 import com.example.teamcity.api.models.User;
+import com.github.viclovsky.swagger.coverage.FileSystemOutputWriter;
+import com.github.viclovsky.swagger.coverage.SwaggerCoverageRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+
+import static com.github.viclovsky.swagger.coverage.SwaggerCoverageConstants.OUTPUT_DIRECTORY;
 
 
 public class Specifications {
@@ -16,6 +21,13 @@ public class Specifications {
 
     private static RequestSpecBuilder reqBuilder() {
         RequestSpecBuilder requestBuilder = new RequestSpecBuilder();
+        requestBuilder.addFilter(new RequestLoggingFilter());
+        requestBuilder.addFilter(new ResponseLoggingFilter());
+        requestBuilder.addFilter(new SwaggerCoverageRestAssured(
+                new FileSystemOutputWriter(
+                        Paths.get("target/" + OUTPUT_DIRECTORY)
+                )
+        ));
         requestBuilder.setBaseUri("http://" + Config.getProperty("host")).build();
         requestBuilder.setContentType(ContentType.JSON);
         requestBuilder.setAccept(ContentType.JSON);
